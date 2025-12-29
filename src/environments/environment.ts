bfbc89a4f8f708f6b8cd2c94eacf9fa7723fa120
Lucas Ferreira <ferreiraplucas@gmail.com>
2025-12-29 15:02:36 +0000

    The key to make everything work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "70759a4f7911402abcc53d3c51d3b759"
 };
 
 /*
